fb93f989ecb42f3e32fef540905ca3dd161bd6ca
MrTiwz <tananphiraphan@outlook.com>
2026-01-30 06:52:35 +0000

    add hls

---
+++ test/src/App.tsx
@@ -0,0 +1,11 @@
+import { VideoPlayer } from '../../src/index'
+
+function App() {
+  return (
+    <div style={{ width: '90%', maxWidth: '1024px', marginInline: 'auto' }}>
+      <VideoPlayer hls source='https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8' />
+    </div>
+  )
+}
+
+export default App
